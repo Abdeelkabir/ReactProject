33b105ac8c266c426cb9c908a473f9243850a95a
Abdelkabir Aarab <amanarabdelkabir01@gmail.com>
2022-11-15 13:14:30 +0100

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import { Navbar, NavbarBrand } from 'reactstrap'
-import Menu from './components/MenuComponent'
-import { DISHES } from './shared/dishes'
-import './App.css'
+import Main from './components/MainComponent'
 
 import React, { Component } from 'react'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      dishes: DISHES,
-    }
-  }
   render() {
     return (
-      <div className='App'>
-        <Navbar dark color='primary'>
-          <div className='container'>
-            <NavbarBrand href='/'>Restorant Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+      <div>
+        <Main />
       </div>
     )
   }
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -58,9 +58,11 @@ class DishDetail extends Component {
     const dishItem = this.renderDish(dish)
     const dishComment = this.renderComments(dish.comments)
     return (
-      <div className='row'>
-        {dishItem}
-        {dishComment}
+      <div className='container'>
+        <div className='row'>
+          {dishItem}
+          {dishComment}
+        </div>
       </div>
     )
   }
